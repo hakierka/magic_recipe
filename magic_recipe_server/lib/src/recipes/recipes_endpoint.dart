5f97eb81c614b637ef28a1a9413dc85f9663785c
hakierka <amy.waliszewska@gmail.com>
2025-07-03 11:52:02 +0200

    Testing generateRecipe & mocking Gemini

--- magic_recipe_server/lib/src/recipes/recipes_endpoint.dart
+++ magic_recipe_server/lib/src/recipes/recipes_endpoint.dart
@@ -2,6 +2,20 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:magic_recipe_server/src/generated/protocol.dart';
 import 'package:serverpod/serverpod.dart';
+import 'package:meta/meta.dart';
+
+@visibleForTesting
+var generateContent =
+    (String apiKey, String prompt) async => (await GenerativeModel(
+          model: 'gemini-1.5-flash-latest',
+          apiKey: apiKey,
+        ).generateContent(
+          [Content.text(prompt)],
+        ))
+            .text;
+
+
+
 
 class RecipesEndpoint extends Endpoint {   
 
@@ -10,18 +24,13 @@ class RecipesEndpoint extends Endpoint {
     if (geminiApiKey == null) {
       throw Exception('Gemini API key not found');
     }
-    final gemini = GenerativeModel(
-      model: 'gemini-1.5-flash-latest', 
-      apiKey: geminiApiKey
-    );
+
     final prompt =
         'Generate a recipe using the following ingredients: $ingredients, always put the title '
         'of the recipe in the first line, and then the instructions. The recipe should be easy '
         'to follow and include all necessary steps. Please provide a detailed recipe.';
 
-    final response = await gemini.generateContent([Content.text(prompt)]);
-
-    final responseText = response.text;
+    final responseText = await generateContent(geminiApiKey, prompt);
 
     if (responseText == null || responseText.isEmpty) {
       throw Exception('No response from Gemini API');
