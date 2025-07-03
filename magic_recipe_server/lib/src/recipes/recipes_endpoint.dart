2940bafca264e57141498b8af9ad6fb4829e4428
hakierka <amy.waliszewska@gmail.com>
2025-07-03 10:32:53 +0200

    create a recipe model

---
+++ magic_recipe_server/lib/src/recipes/recipes_endpoint.dart
@@ -0,0 +1,35 @@
+
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:serverpod/serverpod.dart';
+
+class RecipesEndpoint extends Endpoint {   
+
+  Future<String> generateRecipe(Session session, String ingredients) async {
+    final geminiApiKey = session.passwords['gemini'];
+    if (geminiApiKey == null) {
+      throw Exception('Gemini API key not found');
+    }
+    final gemini = GenerativeModel(
+      model: 'gemini-1.5-flash-latest', 
+      apiKey: geminiApiKey
+    );
+    final prompt =
+        'Generate a recipe using the following ingredients: $ingredients, always put the title '
+        'of the recipe in the first line, and then the instructions. The recipe should be easy '
+        'to follow and include all necessary steps. Please provide a detailed recipe.';
+
+    final response = await gemini.generateContent([Content.text(prompt)]);
+
+    final responseText = response.text;
+    
+    if (responseText == null || responseText.isEmpty) {
+      throw Exception('No response from Gemini API');
+    }
+
+    return responseText;
+
+
+
+  }
+  
+}
